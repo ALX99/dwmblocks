a466474312f7aec11ad3380edac1cf219ab35fb3
ALX99 <46844683+ALX99@users.noreply.github.com>
2020-07-28 18:57:46 +0200

    Config update

--- blocks.h
+++ blocks.h
@@ -1,19 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
-	
-	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "~/bin/statusbar/volume",						0,		10},
-
-	{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
-	
-	{"", "~/bin/statusbar/battery",						5,		0},
-
-	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
-
-	{"", "~/bin/statusbar/clock",						5,		0},
+	//{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,	0},
+	//{"", "sensors | awk '/^temp1:/{print $2}'", 5, 0},
+	{"", "gettraffic", 10, 0},
+	{"", "getconn", 60, 0},
+	{"", "getupdates", 3600, 0},
+    {"", "getvol", 0, 21},
+    {"", "getbri", 0, 20},
+	{"", "getbat", 120, 0},
+	{"", "getdate", 3600, 0},
+	{"", "getclock", 60, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
